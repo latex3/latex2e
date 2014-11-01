debfbc52d37b1a8f1d8e961c809acba172484d9b
Joseph Wright <joseph.wright@morningstar2.co.uk>
2014-11-01 22:42:28 +0000

    Add an l3build script for tools
    There are a lot of .tlg differences here due to various normalisation things:
    they'll need to be checked by altering the Perl normalisation before a 'full'
    switch.
    
    There is one outstanding issue here, again the \asktheoverwrite business.

---
+++ required/tools/build.lua
@@ -0,0 +1,24 @@
+#!/usr/bin/env texlua
+
+-- Build script for LaTeX2e "tools" files
+
+-- Identify the bundle and module
+bundle = ""
+module = "tools"
+
+-- Location of main directory: use Unix-style path separators
+maindir = "../.."
+
+-- Minor modifications to file types
+installfiles = {"*.def", "*.sty"}
+typesetfiles = {"*.dtx", "*.tex"}
+unpackfiles  = {"tools.ins"}
+
+checkdeps = {maindir .. "/base", maindir .. "/required/graphics"}
+
+-- Load the common settings for the LaTeX2e repo
+dofile (maindir .. "/build-config.lua")
+
+-- Find and run the build system
+kpse.set_program_name ("kpsewhich")
+dofile (kpse.lookup ("l3build.lua"))
