ca33c10ff5b0d382c8f500a6e5104a02f7d0a518
Joseph Wright <joseph.wright@morningstar2.co.uk>
2014-11-01 18:18:41 +0000

    A 'bare-bones' l3build script for base
    This is enough to cover the needs of unapcking for 'graphics',
    but not yet enough to actually build base for release.

---
+++ base/build.lua
@@ -0,0 +1,104 @@
+#!/usr/bin/env texlua
+
+-- Build script for LaTeX2e "base" files
+
+-- Identify the bundle and module
+module = "latex2e-base"
+bundle = ""
+
+-- Location of main directory: use Unix-style path separators
+maindir = ".."
+
+-- Set up the file types needed here
+installfiles =
+  {
+    "*.cfg", "*.clo", "*.cls", "*.def", "*.dfu", "*.err", "*.fd", "*.ist",
+    "*.ltx", "*.sty", "*.tex"
+  }
+sourcefiles  = {"*.cls", "*.dtx", "*.fdd", "ltpatch.ltx", "*.ins", "*.tex"}
+typesetfiles =
+  {
+    "alltt.dtx",
+    "classes.dtx",
+    "cmfonts.dtx",
+    "doc.dtx",
+    "docstrip.dtx",
+    "exscale.dtx",
+    "fixltx2e.dtx",
+    "graphpap.dtx",
+    "ifthen.dtx",
+    "inputenc.dtx",
+    "lppl.tex",
+    "utf8ienc.dtx",
+    "latexsym.dtx",
+    "letter.dtx",
+    "ltxdoc.dtx",
+    "makeindx.dtx",
+    "nfssfont.dtx",
+    "proc.dtx",
+    "slides.dtx",
+    "slifonts.dtx",
+    "source2e.tex",
+    "syntonly.dtx",
+  }
+
+-- A few special file for unpacking
+unpackfiles     = {"unpack.ins"}
+unpacksuppfiles = {"hyphen.cfg", "texsys.cfg", "UShyphen.tex"}
+
+-- Custom settings for the check system
+checksuppfiles  = {"latex.fmt"}
+testsuppdir = "testfiles/helpers"
+
+-- No dependencies at all (other than l3build of course)
+checkdeps   = { }
+
+-- A function is needed to build the format
+-- As this is a one-off, things are hard-coded
+function format ()
+  unpack ()
+  -- Much the same as the standard unpack approach: run from 'here' so
+  -- the relationships are all correct
+  os.execute (
+      os_setenv .. " TEXINPUTS=" .. unpackdir .. os_pathsep .. localdir
+      .. os_concat ..
+      "etex -etex -ini " .. " -output-directory=" .. unpackdir ..
+      " " .. unpackdir .. "/latex.ltx"
+    )
+end
+
+-- base does all of the targets itself
+function main (target, file, engine)
+  local errorlevel
+  if target == "check" then
+    check (file, engine)
+  elseif target == "clean" then
+    clean ()
+  elseif target == "ctan" then
+    ctan (true)
+  elseif target == "doc" then
+    doc ()
+  elseif target == "install" then
+    install ()
+  elseif target == "save" then
+    if file then
+      save (file, engine)
+    else
+      help ()
+    end
+  elseif target == "unpack" then
+    -- A simple way to have the unpack target also build the format
+    format ()
+  elseif target == "version" then
+    version ()
+  else
+    help ()
+  end
+end
+
+-- Load the common settings for the LaTeX2e repo
+dofile (maindir .. "/build-config.lua")
+
+-- Find and run the build system
+kpse.set_program_name ("kpsewhich")
+dofile (kpse.lookup ("l3build.lua"))
