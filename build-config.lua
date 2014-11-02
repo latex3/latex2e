1dfbc39b0ac905c7e96633ca5a42b710d36bab91
Joseph Wright <joseph.wright@morningstar2.co.uk>
2014-11-02 10:56:52 +0000

    As test2e sets \scrollmode, the option here is not needed

--- build-config.lua
+++ build-config.lua
@@ -8,7 +8,7 @@ unpackdeps = unpackdeps or {maindir .. "/base"}
 -- Set up the check system to work in 'stand-alone' mode
 -- This relies on a format being built by the 'base' dependency
 checkformat    = ""
-checkopts      = "-interaction=nonstopmode -translate-file ./ascii.tcx -efmt=latex.fmt -output-format=dvi"
+checkopts      = "-translate-file ./ascii.tcx -efmt=latex.fmt -output-format=dvi"
 checkengines   = {"etex"}
 checksuppfiles = {"ascii.tcx", "color.cfg", "graphics.cfg", "test2e.tex"}
 stdengine      = "etex"
