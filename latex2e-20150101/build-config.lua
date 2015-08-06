e2953fca719d60b483d531a7ffe72b9ef05ea744
David Carlisle <d.p.carlisle@gmail.com>
2015-08-06 11:16:43 +0000

    20150101 branch

---
+++ latex2e-20150101/build-config.lua
@@ -0,0 +1,24 @@
+-- Common settings for LaTeX2e development repo
+
+-- The LaTeX2e kernel is needed by everything except 'base'
+-- There is an over-ride for that case
+checkdeps  = checkdeps  or {maindir .. "/base"}
+unpackdeps = unpackdeps or {maindir .. "/base"}
+
+-- Set up the check system to work in 'stand-alone' mode
+-- This relies on a format being built by the 'base' dependency
+checkformat    = "latex"
+checkopts      = ""
+checkengines   = {"xetex","etex"}
+checkruns      = 2
+checksuppfiles = 
+  {"ascii.tcx", "color.cfg", "graphics.cfg", "test209.tex", "test2e.tex", "xetex.def"}
+stdengine      = "etex"
+typesetsuppfiles = {"ltxdoc.cfg", "ltxguide.cfg"}
+
+-- Build TDS-style zips
+packtdszip = true
+
+-- Global searching is disabled when unpacking and checking
+checksearch  = false
+unpacksearch = false
