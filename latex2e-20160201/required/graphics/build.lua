e93370a65b4efd0e0bf731ee57887564e1f571ce
David Carlisle <d.p.carlisle@gmail.com>
2016-02-11 00:58:23 +0000

    20160201 branch

---
+++ latex2e-20160201/required/graphics/build.lua
@@ -0,0 +1,24 @@
+#!/usr/bin/env texlua
+
+-- Build script for LaTeX2e "graphics" files
+
+-- Identify the bundle and module
+bundle = ""
+module = "graphics"
+
+-- CTAN's name for this is a bit different from ours
+ctanpkg = "latex-graphics"
+
+-- Location of main directory: use Unix-style path separators
+maindir = "../.."
+
+-- Minor modifications to file types
+installfiles = {"*.def", "*.sty"}
+typesetfiles = {"*.dtx", "*.tex"}
+
+-- Load the common settings for the LaTeX2e repo
+dofile (maindir .. "/build-config.lua")
+
+-- Find and run the build system
+kpse.set_program_name ("kpsewhich")
+dofile (kpse.lookup ("l3build.lua"))
