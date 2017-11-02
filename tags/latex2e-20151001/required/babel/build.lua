3d7c12d4d7ed0b6863c55d3ff0ee7d12f8371168
Joseph Wright <joseph.wright@morningstar2.co.uk>
2017-11-02 10:22:53 +0000

    Tag '/tags/latex2e-20151001' deleted

--- tags/latex2e-20151001/required/babel/build.lua
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env texlua
-
--- Build script for LaTeX2e "babel" files
-
-dofile("settings.build.lua")
-
--- Find and run the build system
-kpse.set_program_name ("kpsewhich")
-dofile (kpse.lookup ("l3build.lua"))
