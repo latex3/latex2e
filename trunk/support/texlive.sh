32410dbcd51c8bae1de01fdc3ffe1430a9f47dc9
Joseph Wright <joseph.wright@morningstar2.co.uk>
2017-11-02 17:41:59 +0000

    Starting on Travis-CI support
    This covers (hopefully) the base/graphics/tools areas, though babel in
    particular will need additional files. The fact that babel will search
    the tree is the reason latex-bin is included in the core set up (as for
    the LaTeX3 version this is based on).

---
+++ trunk/support/texlive.sh
@@ -0,0 +1,41 @@
+#!/usr/bin/env sh
+
+# This script is used for testing using Travis
+# It is intended to work on their VM set up: Ubuntu 12.04 LTS
+# A minimal current TL is installed adding only the packages that are
+# required
+
+# See if there is a cached version of TL available
+export PATH=/tmp/texlive/bin/x86_64-linux:$PATH
+if ! command -v texlua > /dev/null; then
+  # Obtain TeX Live
+  wget http://mirror.ctan.org/systems/texlive/tlnet/install-tl-unx.tar.gz
+  tar -xzf install-tl-unx.tar.gz
+  cd install-tl-20*
+
+  # Install a minimal system
+  ./install-tl --profile=../support/texlive.profile
+
+  cd ..
+fi
+
+# Needed for any use of texlua even if not testing LuaTeX
+tlmgr install luatex
+
+# The test framework itself
+tlmgr install l3build
+
+# Required to build plain and LaTeX formats:
+# TeX90 plain for unpacking, pdfLaTeX, LuaLaTeX and XeTeX for tests
+tlmgr install cm etex knuth-lib latex-bin tex tex-ini-files unicode-data \
+  xetex
+ 
+# Assuming a 'basic' font set up, metafont is required to avoid
+# warnings with some packages and errors with others
+tlmgr install metafont mfware
+
+# Keep no backups (not required, simply makes cache bigger)
+tlmgr option -- autobackup 0
+
+# Update the TL install but add nothing new
+tlmgr update --self --all --no-auto-install
