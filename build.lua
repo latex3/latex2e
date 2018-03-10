e18d28496f5b2a64bb2c158ad4b28a880748a0ac
Joseph Wright <joseph.wright@morningstar2.co.uk>
2018-03-10 11:05:47 +0000

    Allow build scripts to work with old and new l3build calls
    This supports both
    
         texlua build.lua command
    
    and
    
        l3build command

--- build.lua
+++ build.lua
@@ -74,4 +74,7 @@ end
 -- Find and run the build system
 kpse.set_program_name ("kpsewhich")
 dofile(("./build-config.lua"))
-dofile (kpse.lookup ("l3build.lua"))
+if not release_date then
+  dofile(kpse.lookup("l3build.lua"))
+end
+
