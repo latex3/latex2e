244fab16a6c7e5ee4f08ce7365c0a75b3cd39ca7
Joseph Wright <joseph.wright@morningstar2.co.uk>
2020-01-23 12:23:00 +0000

    Correct copyright step for single-date files

--- build-config.lua
+++ build-config.lua
@@ -93,7 +93,7 @@ function update_tag(file,content,tagname,tagdate)
       "Copyright (C) %1-" .. year .. "\n")
   elseif string.match(content,"%% Copyright %(C%) %d%d%d%d\n") then
     local oldyear = string.match(content,"%% Copyright %(C%) (%d%d%d%d)\n")
-    if not year == oldyear then
+    if not year ~= oldyear then
       content = string.gsub(content,
         "Copyright %(C%) %d%d%d%d\n",
         "Copyright (C) " .. oldyear .. "-" .. year .. "\n")
