05764702b2ad7eff826922f4c8017b496d070764
Frank Mittelbach <frank.mittelbach@latex-project.org>
2020-08-14 00:21:42 +0200

    generalization of config-1run

--- base/config-1run.lua
+++ base/config-1run.lua
@@ -3,6 +3,9 @@
 checksearch  = false
 testfiledir  = "testfiles-1run"
 
-dynamicfiles = {"*.tst","github 220 input.tex","github-0220.sty"}
+dynamicfiles = {"*.tst",
+                "*.aux","*.toc","*.lof","*.lot",          -- may need more depending on tests
+                "github 220 input.tex","github-0220.sty"  -- used by ithub-0220.lvt
+	       }
 
 checkruns     = 1
