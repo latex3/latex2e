2674a9a8dd6504cb837a1106d8b03b1b9cc1dd3b
PhelypeOleinik <tex.phelype@gmail.com>
2020-08-14 22:48:20 -0300

    Test adding tools dependency

--- base/config-lthooks2.lua
+++ base/config-lthooks2.lua
@@ -3,6 +3,11 @@
 checkengines = {"etex"}
 checksearch  = true
 testfiledir  = "testfiles-lthooks2"
+checkdeps    = checkdeps   or
+  {
+    maindir .. "/base",
+    maindir .. "/required/tools"
+  }
 
 -- Custom settings for the check system
 -- testsuppdir = "testfiles-lthooks2/helpers"
