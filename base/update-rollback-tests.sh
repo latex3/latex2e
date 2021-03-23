6ea57f6674d57f45d4212fb0737d31d0423edce1
PhelypeOleinik <phelype.oleinik@latex-project.org>
2021-03-23 11:17:28 -0300

    github-0479-often

--- base/update-rollback-tests.sh
+++ base/update-rollback-tests.sh
@@ -1,8 +1,4 @@
 l3build save -eetex,xetex,luatex \
 	tlb-latexrelease-rollback-003-often \
-	tlb-rollback-004-often
-
-
-l3build save  \
-	github-0479
-
+	tlb-rollback-004-often \
+	github-0479-often
