19413f79829d2b99c149e300e4c7d9e25093f780
Marcel Fabian Krüger <tex@2krueger.de>
2021-05-18 14:58:09 +0200

    stdengine should always be a checkengines member

--- base/config-filenames.lua
+++ base/config-filenames.lua
@@ -1,5 +1,6 @@
 -- Tests for non Unicode engines
 
 checkengines = {"pdftex","xetex","luatex"}
+stdengine    = "pdftex"
 checksearch  = false
 testfiledir  = "testfiles-filenames"
--- base/config-ltcmd.lua
+++ base/config-ltcmd.lua
@@ -1,6 +1,7 @@
 -- Tests for ltcmd (from latex3/l3packages/xparse)
 
 checkengines = {"pdftex","xetex","luatex"}
+stdengine    = "pdftex"
 checksearch  = false
 testfiledir  = "testfiles-ltcmd"
 
