6ed1c19f3db4b702c04938c530ab63f0c24ec6be
Frank Mittelbach <frank.mittelbach@latex-project.org>
2021-11-15 09:57:22 +0100

    need to make the tools directory a dependency because of multicol

--- base/config-doc.lua
+++ base/config-doc.lua
@@ -3,6 +3,13 @@
 -- checkengines = {"etex" }       -- use all engines
 
 checksearch  = true
+
+-- even with search = true we have to load tools to get multicol :-(
+checkdeps =
+  {
+    maindir .. "/required/tools"
+  }
+
 testfiledir  = "./testfiles-doc"
 
 -- testsuppdir  = testfiledir .. "/support"
