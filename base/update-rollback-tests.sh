a6ef0fef01e647f4e87623795e809c8ae0a6f946
Frank Mittelbach <frank.mittelbach@latex-project.org>
2022-02-19 13:01:23 +0100

    implementing \IfBlankTF (3 \let's several hours work :-( }

--- base/update-rollback-tests.sh
+++ base/update-rollback-tests.sh
@@ -1,5 +1,13 @@
 l3build save -eetex,xetex,luatex \
+	tlb-latexrelease-rollback-2020-10-01 \
+	tlb-latexrelease-rollback-2021-06-01 \
+	tlb-latexrelease-rollback-2021-11-15 \
 	tlb-latexrelease-rollback-003-often \
 	tlb-rollback-004-often \
 	tlb-rollback-005 \
 	github-0479-often
+
+
+
+exit
+
