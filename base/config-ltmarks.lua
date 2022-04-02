091c71f4c22d0508944e0410f2cd1bd937276afa
Frank Mittelbach <frank.mittelbach@latex-project.org>
2022-04-02 23:19:52 +0200

    more skeleton stuff

---
+++ base/config-ltmarks.lua
@@ -0,0 +1,13 @@
+-- Tests for ltmarks
+
+checkengines = {"etex"}
+checksearch  = false
+testfiledir  = "testfiles-ltmarks"
+
+-- Custom settings for the check system
+
+--testsuppdir = "testfiles-ltmarks/helpers"
+
+
+checkruns     = 1
+
