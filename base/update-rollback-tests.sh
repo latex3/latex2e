0cebd9a42d13b3af6ca99adcd61018b149d87b0a
Frank Mittelbach <frank.mittelbach@latex-project.org>
2022-04-07 14:43:57 +0200

    include/excluded hooks (#797)
    * first version
    
    * typos fixed - thanks

--- base/update-rollback-tests.sh
+++ base/update-rollback-tests.sh
@@ -2,6 +2,7 @@ l3build save -eetex,xetex,luatex \
 	tlb-latexrelease-rollback-2020-10-01 \
 	tlb-latexrelease-rollback-2021-06-01 \
 	tlb-latexrelease-rollback-2021-11-15 \
+	tlb-latexrelease-rollback-2022-06-01 \
 	tlb-latexrelease-rollback-003-often \
 	tlb-rollback-004-often \
 	tlb-rollback-005 \
