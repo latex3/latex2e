28422122c1fe252b5584b1f2617f8037ea3203d4
Joseph Wright <joseph.wright@morningstar2.co.uk>
2022-08-30 09:23:51 +0200

    Rename usrguide to usrguide-historic

--- base/build.lua
+++ base/build.lua
@@ -106,7 +106,7 @@ typesetfiles   =
     "ltnews??.tex",
     "ltx3info.tex",
     "modguide.tex",
-    "usrguide.tex",
+    "usrguide-historic.tex",
     "usrguide3.tex",
     "latexchanges.tex",
     "*-doc.tex",
