f8d125910087dd9a75fd1f91cbc7545dd0a1add6
Frank Mittelbach <frank.mittelbach@latex-project.org>
2023-03-08 17:03:13 +0100

    regen the tests

--- required/latex-lab/update-mathtagging-all.sh
+++ required/latex-lab/update-mathtagging-all.sh
@@ -1,27 +1,29 @@
+
 l3build save -cconfig-mathtagging \
-        mathcapture-tag-001 \
 	mathcapture-001 \
 	mathcapture-002 \
 	mathcapture-003 \
 	mathcapture-004 \
 	mathcapture-005 \
 	mathcapture-006 \
+	mathcapture-008 \
 	mathcapture-009 \
+	mathcapture-011 \
 	mathcapture-012 \
 	mathcapture-014 \
 	mtag-001 \
+	mtag-002 \
+	mtag-tlc3 \
+        mathcapture-tag-001 \
 	mtag-003
+
+exit
 
 	# with problems:
 	
 l3build save -cconfig-mathtagging \
-	mathcapture-008 \
-	mathcapture-011 \
-	mtag-002 \
-	mtag-tlc3
 
 exit
-
 
 	mathcapture-007 \
 	mathcapture-010 \
