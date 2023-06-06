fe668b0debb083575de2f8626ff401f7388e380e
Ulrike Fischer <fischer@troubleshooting-tex.de>
2023-06-07 00:47:50 +0200

    add support for ltugboat and (local) testfiles

---
+++ required/latex-lab/config-firstaid.lua
@@ -0,0 +1,11 @@
+-- Tests for firstaid
+--
+-- Tests here only function locally due to the class not loaded in github actions!
+
+
+checkengines = {"pdftex"}
+checksearch  = true
+testfiledir  = "testfiles-firstaid"
+
+checkruns     = 4
+
