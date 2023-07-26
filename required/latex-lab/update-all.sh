2d1c1d2916f5e8feab3ca451eb99f5ce0aae8d26
Frank Mittelbach <frank.mittelbach@latex-project.org>
2023-07-26 23:12:53 +0200

    the usual ...

--- required/latex-lab/update-all.sh
+++ required/latex-lab/update-all.sh
@@ -1,10 +1,10 @@
 l3build save \
 	documentmetadata-phase-II \
-	documentmetadata-support-000 \
 	new-or-001 \
 	socket-000
 
 l3build save -epdftex,luatex \
+	documentmetadata-support-000 \
 	documentmetadata-support-001 \
 	documentmetadata-support-002 \
 	socket-001
