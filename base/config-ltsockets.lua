e9f1575b745c050d1ee72f0089cc4e4df2e00b49
Frank Mittelbach <frank.mittelbach@latex-project.org>
2023-08-05 00:47:40 +0200

    first implementation taken from latex-lab

---
+++ base/config-ltsockets.lua
@@ -0,0 +1,13 @@
+-- Tests for ltsockets
+
+checkengines = {"pdftex"}
+checksearch  = false
+testfiledir  = "testfiles-ltsockets
+
+-- Custom settings for the check system
+
+-- testsuppdir = "testfiles-ltsockets/helpers"
+
+
+checkruns     = 1
+
