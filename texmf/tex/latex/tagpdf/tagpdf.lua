c56537c3c340a90df6c6a440b679459fe59063cc
Ulrike Fischer <fischer@troubleshooting-tex.de>
2023-10-27 20:54:34 +0200

    update tagpdf

--- texmf/tex/latex/tagpdf/tagpdf.lua
+++ texmf/tex/latex/tagpdf/tagpdf.lua
@@ -24,8 +24,8 @@
 
 local ProvidesLuaModule = {
     name          = "tagpdf",
-    version       = "0.98l",       --TAGVERSION
-    date          = "2023-08-30", --TAGDATE
+    version       = "0.98m",       --TAGVERSION
+    date          = "2023-10-27", --TAGDATE
     description   = "tagpdf lua code",
     license       = "The LATEX Project Public License 1.3c"
 }
