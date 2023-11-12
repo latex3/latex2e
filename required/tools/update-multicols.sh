1a5b037f576b6e7752ea9542df2fc5c1e84ddf49
Frank Mittelbach <frank.mittelbach@latex-project.org>
2023-11-12 09:45:06 +0100

    exec tst files

---
+++ required/tools/update-multicols.sh
@@ -0,0 +1,11 @@
+l3build save -epdftex \
+    tlb-multicol-001 \
+    tlb-multicol-002 \
+    tlb-multicol-003 \
+    tlb-multicol-004 \
+    tlb-multicol-005 \
+    tlb-multicol-006 \
+    tlb-multicol-007 \
+    tlb-multicol-eisuke \
+    tlb-multicol-rowland
+	
