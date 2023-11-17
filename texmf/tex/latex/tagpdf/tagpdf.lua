a970e5dbd005fe35ec4487504d83a6678c7a32de
Ulrike Fischer <fischer@troubleshooting-tex.de>
2023-11-18 00:33:18 +0100

    add tagpdf 0.98p

--- texmf/tex/latex/tagpdf/tagpdf.lua
+++ texmf/tex/latex/tagpdf/tagpdf.lua
@@ -24,8 +24,8 @@
 
 local ProvidesLuaModule = {
     name          = "tagpdf",
-    version       = "0.98n",       --TAGVERSION
-    date          = "2023-11-10", --TAGDATE
+    version       = "0.98p",       --TAGVERSION
+    date          = "2023-11-17", --TAGDATE
     description   = "tagpdf lua code",
     license       = "The LATEX Project Public License 1.3c"
 }
