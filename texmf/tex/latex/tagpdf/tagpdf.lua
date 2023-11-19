0b3e1e7a3683bd869b326fd6a9fed6fd0d2b02c5
Ulrike Fischer <fischer@troubleshooting-tex.de>
2023-11-19 10:54:27 +0100

    update tagpdf to 0.98q

--- texmf/tex/latex/tagpdf/tagpdf.lua
+++ texmf/tex/latex/tagpdf/tagpdf.lua
@@ -24,8 +24,8 @@
 
 local ProvidesLuaModule = {
     name          = "tagpdf",
-    version       = "0.98p",       --TAGVERSION
-    date          = "2023-11-17", --TAGDATE
+    version       = "0.98q",       --TAGVERSION
+    date          = "2023-11-19", --TAGDATE
     description   = "tagpdf lua code",
     license       = "The LATEX Project Public License 1.3c"
 }
