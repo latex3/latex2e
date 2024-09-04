7a9e6462945ae529683a37bf501dfd9d2d47da1d
Ulrike Fischer <fischer@troubleshooting-tex.de>
2024-09-04 19:17:47 +0200

    Set T1 as default encoding if \DocumentMetadata is used (#1388)
    * Switch to T1 as default with DocumentMetadata
    
    * More testfile updates
    
    * missing braces ...
    
    * update version
    
    * typos from review
    
    * use correct texlive
    
    * preload T1cmtt and T1cmss
    
    * update testfiles
    
    * update tests
    
    * update testfiles
    
    * another testfile
    
    * styling corrections from review
    
    * wording from review

--- required/latex-lab/resave.bat
+++ required/latex-lab/resave.bat
@@ -1,9 +1,1 @@
-  l3build save -c config-OR memoir-001 scrartcl-001
-  l3build save -c config-math mathml-AF-ex1-keys mathcapture-tag-001
-  l3build save -c config-math-luatex mathml-luamml-3 mathml-luamml-2 mathml-luamml-4
-  l3build save -c config-OR-luatex test-minipage scrartcl-001 memoir-001
-  l3build save -c config-sec test-book-2 test-book-1 test-stop-sect-pdf test-article-1 test-suppress-sect
-  l3build save -c config-toc toc-ex-article-hyperref-1 toc-ex-article-no-tagging-hyperref toc-ex-article-hyperref-2 toc-ex-book-tocdepth toc-ex-article-no-hyperref toc-ex-article-no-tagging toc-ex-book-hyperref-1 toc-manual-addcontentsline toc-ex-book-no-hyperref toc-ex-article-hyperref-3
-  l3build save -c config-float marginpar-04-gh-444 marginpar-03 marginpar-01
-  l3build save -c config-table-luatex table-010-longtable-pdf table-010-longtable-pdf2 table-014-pbox-longtable table-009-pdf table-011-endheadbox table-014-pbox table-001-pdf
-  l3build save -c config-title title-004 title-003 title-007 title-008 title-005 title-006 title-009 title-002
+l3build save -c config-table-pdftex table-014-pbox
