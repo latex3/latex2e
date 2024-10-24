3b57ddd2836834121717e81bc0ea518f3535e82d
Ulrike Fischer <fischer@troubleshooting-tex.de>
2024-10-25 01:13:25 +0200

    more examples

--- required/latex-lab/update-math-tagging-examples.sh
+++ required/latex-lab/update-math-tagging-examples.sh
@@ -3,5 +3,6 @@ l3build save -cconfig-math-tagging-examples \
 	math-20-only-mathmlAF \
 	math-20-only-alt-noluamml \
 	math-20-only-texAF-noluamml\
-	math-20-structelem 
+	math-20-structelem\
+	math-20-only-structelem 
 
