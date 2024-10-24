2e6da57210415432dbb1321007bfe64dc726231c
Ulrike Fischer <fischer@troubleshooting-tex.de>
2024-10-24 18:43:45 +0200

    add option for structelem

--- required/latex-lab/update-math-tagging-examples.sh
+++ required/latex-lab/update-math-tagging-examples.sh
@@ -2,5 +2,6 @@ l3build save -cconfig-math-tagging-examples \
 	math-20-default \
 	math-20-only-mathmlAF \
 	math-20-only-alt-noluamml \
-	math-20-only-texAF-noluamml 
+	math-20-only-texAF-noluamml\
+	math-20-structelem 
 
