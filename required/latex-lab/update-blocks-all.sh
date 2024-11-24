3d4d144dac3fbf29195df0fedc90d221b7409316
Frank Mittelbach <frank.mittelbach@latex-project.org>
2024-11-24 12:38:59 +0100

    different approach to handle legacy \listparindent

--- required/latex-lab/update-blocks-all.sh
+++ required/latex-lab/update-blocks-all.sh
@@ -6,16 +6,21 @@ l3build save -cconfig-block -epdftex \
 
 
 l3build save -cconfig-block -epdftex,luatex \
-	blocks-item-01 \
-	blocks-item-02 \
-	blocks-item-03 \
-	blocks-item-04 \
 	blocks-000 \
+	blocks-description-01 \
+	blocks-description-01b \
+	blocks-description-02b \
+	blocks-description-03 \
 	blocks-enumerate-01 \
 	blocks-enumerate-01b \
 	blocks-enumerate-02 \
 	blocks-enumerate-02b \
 	blocks-enumerate-05 \
+	blocks-hyperref-01 \
+	blocks-item-01 \
+	blocks-item-02 \
+	blocks-item-03 \
+	blocks-item-04 \
 	blocks-itemize-01 \
 	blocks-itemize-01b \
 	blocks-itemize-02 \
@@ -25,28 +30,25 @@ l3build save -cconfig-block -epdftex,luatex \
 	blocks-list-02b \
 	blocks-list-03b \
 	blocks-list-04 \
-	blocks-description-01 \
-	blocks-description-01b \
-	blocks-description-02b \
-	blocks-description-03 \
-	blocks-hyperref-01 \
 	blocks-minipage-gh544 \
+        blocks-quote-01 \
+        blocks-quote-02 \
 	blocks-theorem-01 \
 	blocks-theorem-02 \
 	blocks-theorem-03 \
-        blocks-quote-01 \
-        blocks-quote-02 \
-        blocks-trivlist-00 \
+	blocks-theorem-04 \
+	blocks-trivlist-00 \
 	blocks-trivlist-01 \
 	blocks-trivlist-02 \
 	blocks-verbatim-01 \
 	blocks-verbatim-02 \
+	blocks-verbatim-nested-gh119 \
 	blocks-verse-01 \
-	hyperref-target \
-        ptag-001 \
-        problem-blocks-tabbing \
         gallery-III \
         gallery-III-with-sec \
+	hyperref-target \
+        problem-blocks-tabbing \
+        ptag-001 \
 	tagging-0097 \
 	tagging-0097b \
 	tagging-0730
