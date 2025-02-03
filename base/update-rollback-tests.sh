d410e29d9528894d362443f1c04ae836372b9887
Frank Mittelbach <frank.mittelbach@latex-project.org>
2025-02-03 13:34:25 +0100

    typo in script, so missed out one more test

--- base/update-rollback-tests.sh
+++ base/update-rollback-tests.sh
@@ -8,14 +8,14 @@ l3build save -epdftex,xetex,luatex \
 	tlb-latexrelease-rollback-2023-11-01 \
 	tlb-latexrelease-rollback-2024-06-01 \
 	tlb-latexrelease-rollback-2024-11-01 \
-	tlb-latexrelease-rollback-2025-05-01 \
 	tlb-latexrelease-rollback-003-often \
 	tlb-rollback-004-often \
 	tlb-rollback-005 \
 	github-0479-often
 
 l3build save  \
-	tlb-latexrelease-rollback-2023-06-01
+	tlb-latexrelease-rollback-2023-06-01 \
+	tlb-latexrelease-rollback-2025-06-01
 
 l3build save -c config-lthooks \
 	lthooks-rollback-args
