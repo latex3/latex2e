42de997db949f0eff9e43be15f6c5ff37b3868b2
Frank Mittelbach <frank.mittelbach@latex-project.org>
2025-02-14 17:53:36 +0100

    more latex-lab tests

---
+++ required/latex-lab/update-graphic.sh
@@ -0,0 +1,20 @@
+l3build save -cconfig-graphic -epdftex,luatex \
+   graphic-001 \
+   graphic-002 \
+   graphic-003 \
+   graphic-004-angle \
+   graphic-005-trim \
+   graphic-006-width \
+   graphic-007-corr \
+   graphic-008-alt \
+   graphic-009-rotatebox \
+   graphic-010-actualtext \
+   graphic-012-scale \
+   graphic-013-draft \
+   graphic-faults
+
+l3build save -cconfig-graphic -epdftex \
+   graphic-011-picture
+
+
+exit
