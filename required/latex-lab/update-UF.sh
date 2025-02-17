ff7a1484692a948bf1cc4dd53043a162efb0cc9d
Ulrike Fischer <fischer@troubleshooting-tex.de>
2025-02-18 00:34:37 +0100

    set default pdfversion to 2.0

---
+++ required/latex-lab/update-UF.sh
@@ -0,0 +1,6 @@
+l3build save -c config-math-luatex structelem-array-partly-faulty label-xalignat-pdf label-flalign-pdf structelem-sqrt-frac label-gather-pdf label-multline-pdf labelled-align-pdf varlim-commands structelem-text label-align-pdf label-alignat-pdf structelem-mbox
+  l3build save -c config-sec -e luatex test-gh787
+  l3build save -c config-minipage text-phantom
+  l3build save -c config-LM-tagging LM-3-3
+  l3build save -c config-table-pdftex table-001 table-009 table-022-cline table-015 table-020 table-021-longtable table-004-tabularx table-008-disable table-002 table-006-longtable table-005 table-019 table-003 table-007-longtable table-008-multi table-026-multirow table-017
+  l3build save -c config-table-luatex table-015 table-019 table-020 table-001 table-021-longtable table-004-tabularx table-016 table-005 table-002 table-017 table-003 table-018 table-009 table-008-multi table-007-longtable table-006-longtable
