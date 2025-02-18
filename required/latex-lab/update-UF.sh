30d29edbb0461d1b78c16292030b2c64f31fd4f7
Ulrike Fischer <fischer@troubleshooting-tex.de>
2025-02-18 09:53:19 +0100

    sh for testfile updates

---
+++ required/latex-lab/update-UF.sh
@@ -0,0 +1,9 @@
+
+  l3build save -c config-math-luatex fakemath-error math-suspended-gh661
+  l3build save -c config-OR-luatex footmisc-009-multiple footmisc-005 footmisc-009-multiple-tagging footmisc-013-scrartcl tagging-001 footmisc-010-setspace-tagging footmisc-011-para footmisc-003 footmisc-012-side-hyperref footmisc-012-side footmisc-010-setspace footmisc-004
+  l3build save -c config-sec -e luatex test-gh725-opt-arg
+  l3build save -c config-toc -e luatex toc-math-leaders
+  l3build save -c config-block -e luatex problem-blocks-tabbing tagging-0730 blocks-enumerate-01 blocks-hyperref-01 blocks-verse-01 blocks-item-04 blocks-enumerate-01b blocks-verbatim-02 blocks-item-02 hyperref-target blocks-theorem-04 blocks-verbatim-nested-gh119 blocks-item-03 blocks-item-01 blocks-list-04
+  l3build save -c config-graphic -e luatex graphic-005-trim graphic-007-corr graphic-006-width graphic-faults graphic-012-scale graphic-002 graphic-010-actualtext graphic-009-rotatebox graphic-003 graphic-008-alt graphic-001 graphic-004-angle
+  l3build save -c config-float -e luatex float-004 float-007-gh55 float-005-double
+  l3build save -c config-table-luatex table-007-longtable table-003 table-009 table-006-longtable table-013-longtable-hyperref table-002 table-012-caption table-021-longtable table-004-tabularx table-008-multi table-015-presentation table-005 table-001
