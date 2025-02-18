b25ef63066b22ee8fe4aea01ddf147288d0cfe41
Ulrike Fischer <fischer@troubleshooting-tex.de>
2025-02-18 17:27:10 +0100

    update.sh

--- required/latex-lab/update-UF.sh
+++ required/latex-lab/update-UF.sh
@@ -1,14 +1,5 @@
-  l3build save -c config-math-luatex fakemath-error math-suspended-gh661
-  l3build save -c config-OR-luatex footmisc-009-multiple footmisc-005 footmisc-009-multiple-tagging footmisc-013-scrartcl tagging-001 footmisc-010-setspace-tagging footmisc-011-para footmisc-003 footmisc-012-side-hyperref footmisc-012-side footmisc-010-setspace footmisc-004
-  l3build save -c config-sec -e luatex test-gh725-opt-arg
-  l3build save -c config-toc -e luatex toc-math-leaders
-  l3build save -c config-block -e luatex problem-blocks-tabbing tagging-0730 blocks-enumerate-01 blocks-hyperref-01 blocks-verse-01 blocks-item-04 blocks-enumerate-01b blocks-verbatim-02 blocks-item-02 hyperref-target blocks-theorem-04 blocks-verbatim-nested-gh119 blocks-item-03 blocks-item-01 blocks-list-04
-  l3build save -c config-graphic -e luatex graphic-005-trim graphic-007-corr graphic-006-width graphic-faults graphic-012-scale graphic-002 graphic-010-actualtext graphic-009-rotatebox graphic-003 graphic-008-alt graphic-001 graphic-004-angle
-  l3build save -c config-float -e luatex float-004 float-007-gh55 float-005-double
-  l3build save -c config-table-luatex table-007-longtable table-003 table-009 table-006-longtable table-013-longtable-hyperref table-002 table-012-caption table-021-longtable table-004-tabularx table-008-multi table-015-presentation table-005 table-001
-l3build save -c config-math-luatex structelem-array-partly-faulty label-xalignat-pdf label-flalign-pdf structelem-sqrt-frac label-gather-pdf label-multline-pdf labelled-align-pdf varlim-commands structelem-text label-align-pdf label-alignat-pdf structelem-mbox
-  l3build save -c config-sec -e luatex test-gh787
-  l3build save -c config-minipage text-phantom
-  l3build save -c config-LM-tagging LM-3-3
-  l3build save -c config-table-pdftex table-001 table-009 table-022-cline table-015 table-020 table-021-longtable table-004-tabularx table-008-disable table-002 table-006-longtable table-005 table-019 table-003 table-007-longtable table-008-multi table-026-multirow table-017
-  l3build save -c config-table-luatex table-015 table-019 table-020 table-001 table-021-longtable table-004-tabularx table-016 table-005 table-002 table-017 table-003 table-018 table-009 table-008-multi table-007-longtable table-006-longtable
+# l3build check --show-saves -c config-sec test-book-1 test-article-1 test-suppress-sect test-book-2
+#  l3build check --show-saves -c config-toc toc-ex-article-no-hyperref toc-ex-article-hyperref-3 toc-ex-article-hyperref-2 toc-ex-book-tocdepth toc-ex-book-no-hyperref toc-ex-article-hyperref-1 toc-ex-book-hyperref-1
+#  l3build check --show-saves -c config-minipage minipage-006-gh723
+#  l3build check --show-saves -c config-float float-sidewaystable marginpar-04-gh-444 marginpar-03
+#  l3build check --show-saves -c config-firstaid test-amsart-title
