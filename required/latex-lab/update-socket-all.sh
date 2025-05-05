ec9215f45d61c2c8eb4211864d9bf101659d84c7
Ulrike Fischer <fischer@troubleshooting-tex.de>
2025-05-05 11:44:32 +0200

    update testfiles (new socket)

--- required/latex-lab/update-socket-all.sh
+++ required/latex-lab/update-socket-all.sh
@@ -3,5 +3,7 @@ l3build save -cconfig-LM-tagging \
 
 sh update-OR-all.sh
 
+l3build save -c config-table-luatex table-006-longtable table-021-longtable table-019 table-001 table-020 table-003 table-018 table-008-multi table-017 table-016 table-005 table-015 table-004-tabularx table-007-longtable table-009 table-002
+l3build save -c config-table-pdftex table-020 table-015 table-005 table-008-disable table-022-cline table-002 table-007-longtable table-021-longtable table-026-multirow table-004-tabularx table-006-longtable table-008-multi table-001 table-019 table-017 table-009 table-003
 
 exit
