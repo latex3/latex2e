9530c25cff437faa5aaea192dac49576b1907dca
Ulrike Fischer <fischer@troubleshooting-tex.de>
2025-06-24 16:09:13 +0200

    save update file

--- required/latex-lab/update-UF.sh
+++ required/latex-lab/update-UF.sh
@@ -1,5 +1,4 @@
-# l3build check --show-saves -c config-sec test-book-1 test-article-1 test-suppress-sect test-book-2
-#  l3build check --show-saves -c config-toc toc-ex-article-no-hyperref toc-ex-article-hyperref-3 toc-ex-article-hyperref-2 toc-ex-book-tocdepth toc-ex-book-no-hyperref toc-ex-article-hyperref-1 toc-ex-book-hyperref-1
-#  l3build check --show-saves -c config-minipage minipage-006-gh723
-#  l3build check --show-saves -c config-float float-sidewaystable marginpar-04-gh-444 marginpar-03
-#  l3build check --show-saves -c config-firstaid test-amsart-title
+l3build save -e luatex new-or-001 footmisc-19
+l3build save -c config-math mathml-AF-ex1-keys
+
+l3build check --show-saves documentmetadata-support-000 documentmetadata-tagging documentmetadata-support-001 documentmetadata-latest documentmetadata-support-002
