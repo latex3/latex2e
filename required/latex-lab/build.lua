0a5bc2101c600d59ea7b0d98f4fc8c861ec99f86
user227621 <74978909+user227621@users.noreply.github.com>
2025-06-24 10:46:46 +0200

    Minor followup to #1583 (#1727)
    * Correct the GitHub number
    
    * Split the entries for m? and ?m into individual macrocode blocks, analogous to the other entries
    
    * Move one block for m weight rules between the blocks for sl weight rules and sb weight rules
    
    * Rewording
    
    * Unify spaces
    
    * Update version and date string
    
    * fix for tagging/845 (#1784)
    
    * fix for tagging/845
    
    * typo
    
    * text adjustment after review
    
    * Fix amsmath timing for some packages in latex-lab-mathpkg (#1772)
    
    * amsmath timing
    
    Update latex-lab-mathpkg to load amsmath before packages that error otherwise
    
    * one more
    
    * remove space
    
    * add changes.txt entry
    
    * sync dates
    
    * move redefinition of \measuring@true to begin document, latex3/tagging-project#920 (#1788)
    
    * Split documentation runs for GitHub actions
    
    * Forgot one \changes entry
    
    * Offer 'm' as alternative result series for 'm?'/'?m' rules
    
    * Update changes.txt
    
    * ltnews entry (WIP)
    
    * Keep entry
    
    * Correct another reference to the wrong (non-merged) PR
    
    * Improve ltnews entry
    
    ---------
    
    Co-authored-by: Frank Mittelbach <frank.mittelbach@latex-project.org>
    Co-authored-by: mbertucci47 <61854785+mbertucci47@users.noreply.github.com>
    Co-authored-by: Ulrike Fischer <fischer@troubleshooting-tex.de>
    Co-authored-by: Marcel Fabian Krüger <tex@2krueger.de>

--- required/latex-lab/build.lua
+++ required/latex-lab/build.lua
@@ -17,11 +17,53 @@ installfiles = {
 		 "glyphtounicode-cmex.tex",
          "tagpdf-ns-latex-lab.def"  
 		}
-typesetfiles = {
-                 "latex-lab-*.dtx",
-                 "*-doc.tex",
-		 "*-code.tex",
-	       }
+typesetfiles_list = {
+  {
+    "blocks-code.tex",
+    "blocks-doc.tex",
+    "documentmetadata-support-code.tex",
+    "documentmetadata-support-doc.tex",
+    "latex-lab-amsmath.dtx",
+    "latex-lab-namespace.dtx",
+    "latex-lab-sec.dtx",
+  },
+  {
+    "latex-lab-graphic.dtx",
+    "latex-lab-l3doc-tagging.dtx",
+    "latex-lab-marginpar.dtx",
+    "latex-lab-math.dtx",
+    "latex-lab-mathintent.dtx",
+    "latex-lab-mathpkg.dtx",
+    "latex-lab-mathtools.dtx",
+    "latex-lab-minipage.dtx",
+    "latex-lab-new-or-1.dtx",
+    "latex-lab-new-or-2.dtx",
+    "latex-lab-float.dtx",
+    "latex-lab-firstaid.dtx",
+  },
+  {
+    "latex-lab-table.dtx",
+    "latex-lab-testphase.dtx",
+    "latex-lab-text.dtx",
+    "latex-lab-tikz.dtx",
+    "latex-lab-title.dtx",
+    "latex-lab-toc.dtx",
+    "latex-lab-toc-hyperref-changes.dtx",
+    "latex-lab-toc-kernel-changes.dtx",
+    "latex-lab-unicode-math.dtx",
+    "latex-lab-footnotes.dtx",
+    "latex-lab-bib.dtx",
+  }
+}
+local doc_component_setting = os.getenv'LTX_DOC_COMPONENT'
+if doc_component_setting then
+  typesetfiles = typesetfiles_list[math.tointeger(doc_component_setting)]
+else
+  typesetfiles = {}
+  for _, files in ipairs(typesetfiles_list) do
+    table.move(files, 1, #files, #typesetfiles + 1, typesetfiles)
+  end
+end
 
 
 
