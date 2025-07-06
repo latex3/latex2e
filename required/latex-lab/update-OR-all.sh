aa474bf4eba407822a64baa73d72a2d2561e340a
Ulrike Fischer <fischer@troubleshooting-tex.de>
2025-07-06 15:48:07 +0200

    update/correct sh file

--- required/latex-lab/update-OR-all.sh
+++ required/latex-lab/update-OR-all.sh
@@ -2,11 +2,12 @@
 
 l3build save -cconfig-OR \
 	footmisc-005 \
-	memoir-001 \    
-	scrartcl-001 \    
-	tagging-001 \    
+	footmisc-005-tagging-off \
+	memoir-001 \
+	scrartcl-001 \
+	tagging-001 \
 	tagging-002-longtable \
-	tlb-multicol-marks-tagged 
+	tlb-multicol-marks-tagged
 
 
 l3build save -cconfig-OR-luatex \
