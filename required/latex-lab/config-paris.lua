8f9191d8ea7f82ca206b790d6f5d5fa1b0f719c7
Frank Mittelbach <frank.mittelbach@latex-project.org>
2025-07-10 16:11:16 +0200

    leftover tests [ci skip]

---
+++ required/latex-lab/config-paris.lua
@@ -0,0 +1,11 @@
+-- Tests for paris23 tagging stuff
+
+stdengine    = "pdftex"
+checkengines = {"pdftex","luatex"}
+checksearch  = true
+testfiledir  = "testfiles-paris"
+
+checkruns     = 4
+forcecheckruns = true  --- for tagged stuff
+
+
---
+++ required/latex-lab/update-paris.sh
@@ -0,0 +1,5 @@
+l3build save -epdftex,luatex -cconfig-paris \
+	quote-01 \
+	theorem-01 \
+	theorem-02 \
+	verbatim-01
