1ac9db2381f65f0010b1e2cfb4cb774e59a9ab3a
Frank Mittelbach <frank.mittelbach@latex-project.org>
2025-12-21 23:23:59 +0100

    Gh1910 (#1939)
    * fix for #1910
    
    * more test file changes ...
    
    * one more [ci skip]
    
    * add news entry; reformat other entries (only linebreaking)
    
    * Apply suggestion from @muzimuzhi
    
    Co-authored-by: Yukai Chou <muzimuzhi@gmail.com>
    
    * correction after review
    
    * Update base/doc/ltnews43.tex
    
    Co-authored-by: David Carlisle <d.p.carlisle@gmail.com>
    
    ---------
    
    Co-authored-by: Yukai Chou <muzimuzhi@gmail.com>
    Co-authored-by: David Carlisle <d.p.carlisle@gmail.com>

--- base/update-rollback-tests.sh
+++ base/update-rollback-tests.sh
@@ -9,15 +9,12 @@ l3build save -epdftex,xetex,luatex \
 	tlb-latexrelease-rollback-2024-06-01 \
 	tlb-latexrelease-rollback-2024-11-01 \
 	tlb-latexrelease-rollback-2025-06-01 \
+	tlb-latexrelease-rollback-2025-11-01 \
 	tlb-latexrelease-rollback-003-often \
 	tlb-latexrelease-rollback-004 \
 	tlb-rollback-004-often \
 	tlb-rollback-005 \
 	github-0479-often
-
-l3build save  \
-	tlb-latexrelease-rollback-2023-06-01 \
-	tlb-latexrelease-rollback-2025-11-01
 
 l3build save -c config-lthooks \
 	lthooks-rollback-args
