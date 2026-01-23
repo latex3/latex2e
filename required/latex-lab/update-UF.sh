6484ad99ba8624de9061060e42b6ca89dcf63563
Ulrike Fischer <fischer@troubleshooting-tex.de>
2026-01-23 19:47:15 +0100

    update testfiles (metadata changes)

--- required/latex-lab/update-UF.sh
+++ required/latex-lab/update-UF.sh
@@ -1,2 +1,17 @@
+ l3build save -eluatex,pdftex standard-a4f
+ l3build save -c config-OR tagging-002-longtable scrartcl-001 memoir-001
+ l3build save -c config-math mathcapture-tag-001 mtag-008-gh765 mathml-AF-ex1-keys
+ l3build save -c config-math-luatex label-gather-pdf math-20-alt-noluamml labelled-align-pdf amsmath-mbox label-xalignat-pdf label-multline-pdf label-flalign-pdf fakemath mathml-luamml-2 lua-uni-varlim-commands mathml-luamml-MSFT structelem-sqrt-frac varlim-commands structelem-text structelem-mbox mathml-luamml-4 mtag-008-gh765 structelem-array-partly-faulty mathml-luamml-3 array label-alignat-pdf label-align-pdf math-20-structelem lua-uni-structelem-sqrt-frac
+  l3build save -c config-OR-luatex memoir-001 test-minipage scrartcl-001
+l3build save -eluatex,pdftex -c config-sec test-article-1 test-stop-sect-pdf test-book-tagging-off test-book-structure-dest test-book-2 test-suppress-sect test-book-3 test-sec-keys test-book-1
+l3build save -eluatex,pdftex -c config-toc toc-ex-book-hyperref-1 toc-manual-addcontentsline toc-ex-book-tocdepth toc-ex-book-no-hyperref toc-ex-article-no-tagging-hyperref toc-ex-article-no-tagging toc-ex-article-no-hyperref toc-ex-article-hyperref-3 toc-ex-article-hyperref-2 toc-ex-article-hyperref-1
+ l3build save -eluatex,pdftex -c config-graphic tikz-005 tikz-007 tikz-006 tikz-003 graphic-rotating tikz-004 tikz-001 tikz-002
+ l3build save -eluatex,pdftex -c config-float marginpar-04-gh-444 float-013-new float-sidewaystable marginpar-01 marginpar-03 float-015-defer float-012-unknown float-014-defer
+ l3build save -eluatex,pdftex -c config-bib bib-010-natbib bib-006 bib-007-natbib bib-009 bib-008-natbib bib-005
+  l3build save -c config-table-pdftex table-014-pbox-longtable table-014-pbox table-027-longtable-554 table-011-endheadbox table-010-longtable-pdf table-009-pdf table-023-multirow table-001-pdf table-026-minipage-37 table-025 table-024-multirow table-020-rowheader table-010-longtable-pdf2
+
+l3build save -c config-table-luatex tagging-965 table-011-endheadbox table-014-pbox-longtable table-014-pbox table-010-longtable-pdf table-009-pdf table-010-longtable-pdf2 table-001-pdf
+l3build save -eluatex,pdftex -c config-title title-002 title-008 title-009 title-007 title-006 title-005 title-003 title-004
+ l3build save -c config-firstaid test-booktabs test-amsart-title firstaid-amsproc firstaid-amsbook
 
 
