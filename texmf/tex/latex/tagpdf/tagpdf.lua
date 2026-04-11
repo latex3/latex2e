e23a7359f5f077714684ce435d7b9243d2d3f25c
Ulrike Fischer <fischer@troubleshooting-tex.de>
2026-04-11 10:10:42 +0200

    update tagpdf to 1.0a

--- texmf/tex/latex/tagpdf/tagpdf.lua
+++ texmf/tex/latex/tagpdf/tagpdf.lua
@@ -24,8 +24,8 @@
 
 local ProvidesLuaModule = {
     name          = "tagpdf",
-    version       = "0.99z",       --TAGVERSION
-    date          = "2026-03-19", --TAGDATE
+    version       = "1.0a",       --TAGVERSION
+    date          = "2026-04-11", --TAGDATE
     description   = "tagpdf lua code",
     license       = "The LATEX Project Public License 1.3c"
 }
