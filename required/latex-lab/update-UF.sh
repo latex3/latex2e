69f58672a908f6e35a73a6f6bdd6c6540ae87bc6
Ulrike Fischer <fischer@troubleshooting-tex.de>
2026-04-27 22:14:53 +0200

    Review of bib and text module (#2059)
    * revise bib implementation. Handle issue latex3/tagging-project#1282 and Handle issue latex3/tagging-project#1293
    
    * update date.
    
    * Use tagging sockets. Tag sub- and superscript. Suppress space char with luatex. Tag \verb.
    
    * Add attributes
    
    * correct guard
    
    * update testfiles
    
    * move math attributes to latex-lab-namespace.dtx
    
    * use symbolic name for \verb
    
    * extend LM-tests
    
    * update testfiles
    
    * miss a luatex testfile
    
    * use pkg everywhere
    
    * from review
    
    * remove unneeded hyperref hooks.
    
    * from review

--- required/latex-lab/update-UF.sh
+++ required/latex-lab/update-UF.sh
@@ -1,8 +1,10 @@
-l3build save -c config-OR footmisc-005-tagging-off footmisc-005 tagging-001
-l3build save -c config-math mathcapture-tag-001 mtag-005-intertext
-l3build save -c config-OR-luatex footmisc-013-scrartcl footmisc-009-multiple-tagging test5 tagging-001 footmisc-011-para memoir-001 footmisc-012-side-hyperref test9 scrartcl-001 footmisc-012-side test2 test8 test-tnote-setup test6 test-minipage test1 test3 test4 test11-series footmisc-010-setspace footmisc-005 footmisc-004 test7 footmisc-010-setspace-tagging test10 footmisc-009-multiple footmisc-003
-l3build save -eluatex,pdftex -c config-minipage minipage-005-footnote
-l3build save -c config-footnote footmisc-012-side footmisc-floats-001 footmisc-011-para footmisc-012-side-hyperref footmisc-003 footmisc-floats-000 footmisc-floats-004 tagging-845 footmisc-008-pdfcol footmisc-009-multiple footnote-float-above footmisc-002 footmisc-floats-003 footmisc-006 footmisc-009-multiple-tagging footmisc-floats-abovefloats-flushbottom footmisc-004 footmisc-floats-abovefloats footmisc-floats-005 footmisc-010-setspace footmisc-floats-002 footmisc-013-scrartcl footmisc-010-setspace-tagging footmisc-005 footmisc-007-rollback
-l3build save -c config-table-pdftex table-005 table-000
-l3build save -c config-table-luatex table-005 table-000
-l3build save -c config-title -e luatex,pdftex title-001 title-004 title-002 title-007 title-005 title-006 title-009 title-003 title-008
+l3build save eluatex,pdftex footmisc-19
+l3build save -c config-OR footmisc-005 scrartcl-001 memoir-001 tagging-001 tagging-002-longtable
+l3build save -c config-math mtag-002 mathcapture-002
+ l3build save -c config-math-luatex tagging-957 smash
+  l3build save -c config-OR-luatex test9 test8 test3 test11-series footmisc-013-scrartcl footmisc-004 test6 footmisc-010-setspace-tagging footmisc-009-multiple-tagging memoir-001 scrartcl-001 tagging-001 test7 footmisc-003 test-minipage test-tnote-setup test2 footmisc-005 footmisc-011-para test10 test1 test4 test5
+l3build save -c config-minipage minipage-005-footnote
+l3build save -c config-footnote footmisc-003 footmisc-011-para footnote-heading footmisc-004 footmisc-floats-002 footmisc-009-multiple-tagging footmisc-002 footmisc-floats-000 tagging-845 footnote-float-above footmisc-005 footmisc-010-setspace-tagging footmisc-floats-003 footmisc-floats-005 footmisc-006 footmisc-floats-001 footmisc-floats-004 footmisc-013-scrartcl
+ l3build save -c config-LM-tagging LM-2-2
+l3build save -c config-title title-006 title-005 title-007 title-008 title-009 title-004 title-002 title-003
+ l3build save -c config-firstaid test-ltugboat firstaid-microtype test-amsart-title
