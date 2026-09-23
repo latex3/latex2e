3478e947304c93a4029a4ad9d49007b3ca925970
Frank Mittelbach <frank.mittelbach@latex-project.org>
2026-09-23 12:48:50 +0200

    try once more

--- base/build.lua
+++ base/build.lua
@@ -71,7 +71,7 @@ textfiles =
     "lppl-1-1.txt",
     "lppl-1-2.txt",
   }
-typesetsourcefiles = { "ltnews??.tex" }  -- needed to be there for ltnews.tex
+typesetsuppfiles = { "ltnews??.tex" }  -- needed to be there for ltnews.tex
 typesetfiles_list = {
   {
     "source2e.tex", -- Has to be first: source2e.ist creation!
